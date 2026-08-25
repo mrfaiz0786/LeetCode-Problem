f417dde6cabfed5ca7101d1e7d108a094883e65c
Md. Faiz Ansari <mrfaizansari111@gmail.com>
2026-08-25 22:51:12 +0530

    Time: 3 ms (25.63%), Space: 54.6 MB (67.15%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,26 +4,26 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    // let set = new Set();
-    // let answer = new Set();
-    // for(let i=0; i<nums1.length;i++){
-    //     set.add(nums1[i])
-    // }
-    // for(let j=0;j<nums2.length;j++){
-    //    if( set.has(nums2[j])){
-    //     answer.add(nums2[j])
-    //    }
-    // }
-    // return [...answer]
-    let res = [];
-    for(let i=0; i<nums1.length; i++){
-        for(let j=0; j<nums2.length; j++)
-            if(nums1[i] == nums2[j]){
-                if(!res.includes(nums1[i])){
-                    res.push(nums1[i])
-                }
-            }
-        
+    let set = new Set();
+    let answer = new Set();
+    for(let i=0; i<nums1.length;i++){
+        set.add(nums1[i])
     }
-    return res
+    for(let j=0;j<nums2.length;j++){
+       if( set.has(nums2[j])){
+        answer.add(nums2[j])
+       }
+    }
+    return [...answer]
+    // let res = [];
+    // for(let i=0; i<nums1.length; i++){
+    //     for(let j=0; j<nums2.length; j++)
+    //         if(nums1[i] == nums2[j]){
+    //             if(!res.includes(nums1[i])){
+    //                 res.push(nums1[i])
+    //             }
+    //         }
+        
+    // }
+    // return res
 };
