513eb48ce1079a4b79c569474cf8cfcb7f9fc9f7
Md. Faiz Ansari <mrfaizansari111@gmail.com>
2026-08-29 00:41:49 +0530

    Time: 1 ms (48.93%), Space: 58.4 MB (26.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,14 +4,18 @@
  */
 var removeDuplicates = function (nums) {
     let i = 0;
-    for (j = 0; j < nums.length; j++) {
+
+    for (let j = 0; j < nums.length; j++) {
+
         if (nums[i] < nums[j]) {
-           let temp = nums[i+1]
-           nums[i+1] = nums[j]
-           nums[j] = temp
-           i++
+
+            // new unique element mila
+            i++;
+
+            nums[i] = nums[j];
         }
     }
-        return i+1
+
+    return i + 1;
 
 };
