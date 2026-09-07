0dc762bc325ee55458a2dc7751f0a258d0dfe270
Md. Faiz Ansari <mrfaizansari111@gmail.com>
2026-09-07 14:20:53 +0530

    Time: 7 ms (36.85%), Space: 96.6 MB (15.49%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+     k = k % nums.length;
+     function reverse(left, right){
+        while(left<right){
+            let temp = nums[left]
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+     }
+     reverse(0,nums.length-1)
+     reverse(0,k-1)
+     reverse(k,nums.length-1)
+};
